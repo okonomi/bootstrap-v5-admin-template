744c6278ac75163ed3cf8931b724597713418926
okonomi <okonomi@oknm.jp>
2020-11-23 23:11:01 +0900

    feat: toggle sidebar

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/css/all.css'
 import './admin.scss'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sidebar = document.getElementById('sidebar')
+  const toggle = document.getElementById('sidebar-toggle')
+  toggle.addEventListener('click', (e) => {
+    sidebar.classList.toggle('visually-hidden')
+  })
+})
